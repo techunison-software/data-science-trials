819a979ea947b2e5fe343959f26f070c19437ac7
Prasanna <prasanna@techunison.com>
2019-07-22 10:11:06 +0530

    Titanic-2

--- Prasanna/Titanic/LinearReg.py
+++ Prasanna/Titanic/LinearReg.py
@@ -1,30 +1,65 @@
 import pandas as pd
-import numpy as np
-import random as rnd
 import seaborn as sns
-import matplotlib.pyplot as plt
+import statsmodels.formula.api as smf
+from sklearn.linear_model import LinearRegression
+from sklearn import metrics
+#from sklearn.cross_validation import train_test_split
+import numpy as np
 
 
-from sklearn.linear_model import LogisticRegression
-from sklearn.svm import SVC, LinearSVC
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.naive_bayes import GaussianNB
-from sklearn.linear_model import Perceptron
-from sklearn.linear_model import SGDClassifier
-from sklearn.tree import DecisionTreeClassifier
-
-sns.set_palette('viridis')
-sns.set_style('whitegrid')
-
-train_df = pd.read_csv('train.csv')
+train_df = pd.read_csv('train.csv',index_col=0)
 test_df = pd.read_csv('test.csv')
 combine = [train_df, test_df]
+#print(train_df.info())
 
-train_df[['Pclass', 'Survived']].groupby(['Pclass'], as_index=False).mean().sort_values(by='Survived', ascending=False)
-train_df[["Sex", "Survived"]].groupby(['Sex'], as_index=False).mean().sort_values(by='Survived', ascending=False)
-train_df[["SibSp", "Survived"]].groupby(['SibSp'], as_index=False).mean().sort_values(by='Survived', ascending=False)
-train_df[["Parch", "Survived"]].groupby(['Parch'], as_index=False).mean().sort_values(by='Survived', ascending=False)
+#malesex_train_df = train_df[['Sex','Survived']].groupby(['Sex'],as_index=False).mean().sort_values(by='Sex', ascending=True)
+#print(malesex_train_df)
 
-g = sns.FacetGrid(train_df, col='Survived')
-print(g.map(plt.hist, 'Age', bins=20))
+#StatsModel
+
+#lm1 = smf.ols(formula='Survived ~  Sex+ Name', data=train_df).fit()
+lm1 = smf.ols(formula='Survived ~ Sex', data=train_df).fit()
+#print(lm1.params)
+#print(lm1.conf_int())
+#print(lm1.pvalues)
+#print(lm1.rsquared)
+#print(lm1.summary())
+
+# for dataset in combine:
+#     dataset['Sex'] = dataset['Sex'].map( {'female': 1, 'male': 0} ).astype(int)
+
+
+for dataset in combine:
+    dataset['Sex'] = dataset['Sex'].map( {'female': 1, 'male': 0} ).astype(int)
+
+X_train = train_df['Sex']
+Y_train = train_df["Survived"]
+X_test  = test_df['Sex']
+
+lm2 = LinearRegression()
+lm2.fit(X_train, Y_train)
+y_pred = lm2.predict(X_test)
+final_pred = np.around(y_pred)
+acc_log = round(lm2.score(X_train, Y_train) * 100, 2)
+print(acc_log)
+
+# logreg = LogisticRegression()
+# logreg.fit(X_train, Y_train)
+# Y_pred = logreg.predict(X_test)
+# acc_log = round(logreg.score(X_train, Y_train) * 100, 2)
+# print(acc_log)
+
+
+#Scikit-learn
+# for dataset in combine:
+#     dataset['Sex'] = dataset['Sex'].map( {'female': 1, 'male': 0} ).astype(int)
+
+# feature_cols = ['Sex']
+# X = train_df[feature_cols]
+# y = train_df.Survived
+# lm2 = LinearRegression()
+# lm2.fit(X, y)
+#print(lm2.intercept_)
+#print(lm2.coef_)
+
+
