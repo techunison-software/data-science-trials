0522b5b41fb396f2aa66c9067b598961f5d7a245
Prasanna <prasanna@techunison.com>
2019-07-23 10:12:43 +0530

    LinearReg

--- Prasanna/Titanic/LinearReg.py
+++ Prasanna/Titanic/LinearReg.py
@@ -8,8 +8,8 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.naive_bayes import GaussianNB
 from sklearn.linear_model import Perceptron
-from sklearn.linear_model import SGDClassifier
-from sklearn.tree import DecisionTreeClassifier
+# from sklearn.linear_model import SGDClassifier
+# from sklearn.tree import DecisionTreeClassifier
 from sklearn import metrics
 #from sklearn.cross_validation import train_test_split
 import numpy as np
