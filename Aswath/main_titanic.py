d00571e4d7d86346acbbda313ac8e787198a3537
tuiaswath <50611592+tuiaswath@users.noreply.github.com>
2019-07-29 08:19:37 +0530

    Cleaning the code
    Revision to clean the code

--- Aswath/main_titanic.py
+++ Aswath/main_titanic.py
@@ -4,6 +4,7 @@ from sklearn.preprocessing import LabelEncoder
 from sklearn.impute import SimpleImputer
 import inspect, os.path
 from sklearn.linear_model import LinearRegression
+import statsmodels.api as sm
 
 filename = inspect.getframeinfo(inspect.currentframe()).filename
 trainpath = os.path.dirname(os.path.abspath(filename))+"/datasets/titanic/train.csv"
@@ -23,8 +24,8 @@ X_train[:, 2:3] = imp_mean.transform(X_train[:, 2:3])
 
 imp_mean = imp_mean.fit(X_test[:, 2:3])
 X_test[:, 2:3] = imp_mean.transform(X_test[:, 2:3])
-
 test_data['Age']=X_test[:,2]
+test_data['Fare'].fillna(round(test_data['Fare'].mean()),inplace=True)
 labelencoder_x = LabelEncoder()
 X_train[:, 1] = labelencoder_x.fit_transform(X_train[:, 1].astype(str))
 #X_train[:, 3] = labelencoder_x.fit_transform(X_train[:, 3].astype(str))
@@ -46,16 +47,19 @@ reg = linear_model.LinearRegression()
 reg.fit(y_train.reshape(-1, 1), X_opt)
 
 regressor_OLS = sm.OLS(endog=y_train, exog=X_opt.astype(float)).fit()
-regressor_OLS.summary()
+print("Modal Statistics with all the 3 features")
+print(regressor_OLS.summary())
 
 X_opt = X_train[:, [0, 1, 2]]
 regressor_OLS = sm.OLS(endog=y_train, exog=X_opt.astype(float)).fit()
-regressor_OLS.summary()
+print("Modal Statistics with 2 of 3 features")
+print(regressor_OLS.summary())
 
 X_opt = X_train[:, [0, 1]]
 regressor_OLS = sm.OLS(endog=y_train, exog=X_opt.astype(float)).fit()
-regressor_OLS.summary()
-test_data = pd.read_csv(testpath)
+print("Modal Statistics with 1 of 3 features")
+print(regressor_OLS.summary())
+#test_data = pd.read_csv(testpath)
 test_data['Survived']=final_pred
 test_data=test_data.drop('Cabin',axis=1)
 df = pd.DataFrame({'PassengerId': pd.Series(range(892, 1310)), 'Survived': final_pred})
